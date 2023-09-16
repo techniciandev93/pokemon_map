8a33d5d850953cd6977825d6143232fb7988c4f4
anton <anton@adminpc.Dlink>
2023-09-16 15:25:54 +0300

    внесены исправления в моделях

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -6,28 +6,24 @@ class Pokemon(models.Model):
     title_en = models.CharField(max_length=200, verbose_name='Наименование на Английском', blank=True)
     title_jp = models.CharField(max_length=200, verbose_name='Наименование на Японском', blank=True)
     description = models.TextField(verbose_name='Описание', blank=True)
-    evolution_pokemon = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True,
-                                          related_name='next_evolution_pokemon',
-                                          verbose_name='От кого эволюционировал')
+    previous_evolution = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True,
+                                           related_name='next_evolutions',
+                                           verbose_name='От кого эволюционировал')
     image = models.ImageField(upload_to='images/pokemons', null=True, blank=True, verbose_name='Изображение')
 
     def __str__(self):
         return self.title_ru
 
     @property
-    def previous_evolution(self):
-        return self.evolution_pokemon
-
-    @property
     def next_evolution(self):
-        return self.next_evolution_pokemon.first()
+        return self.next_evolutions.first()
 
 
 class PokemonEntity(models.Model):
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон',
-                                related_name='pokemons')
+                                related_name='appeared_pokemons')
     appeared_at = models.DateTimeField(verbose_name='Дата появления')
     disappeared_at = models.DateTimeField(verbose_name='Дата исчезновения')
     level = models.IntegerField(verbose_name='Уровень', null=True, blank=True)
